b94988c123381fd69927166829f143edfc9ebcb7
unknown <rachael.kate.stokes@gmail.com>
2024-03-29 11:54:21 -0400

    fixed some errors in main.jsx

--- portfolioRefresh/src/main.jsx
+++ portfolioRefresh/src/main.jsx
@@ -1,11 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import  ReactDOM  from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import App from './App';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);

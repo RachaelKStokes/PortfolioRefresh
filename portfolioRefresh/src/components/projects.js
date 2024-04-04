4388aae3551d1120e5f06036756046ade5b145ee
unknown <rachael.kate.stokes@gmail.com>
2024-04-04 19:39:53 -0400

    added projects

---
+++ portfolioRefresh/src/components/projects.js
@@ -0,0 +1,36 @@
+const projects = [
+    {
+        id: "1",
+        title: "Logo Generator",
+        picture: "",
+        used: "JavaScript, OOP, NodeJS",
+        repoLink: "https://github.com/RachaelKStokes/WakeMeUp.BeforeYouLogo"
+        
+    },
+    { 
+        id: "2",
+        title: "ReadMe Generator",
+        picture: "",
+        used: "JavaScript, NodeJS, MarkDown",
+        repoLink: "https://github.com/RachaelKStokes/ReadMe.IfYouDare"
+        
+    },
+    {
+        id: "3",
+        title: "Employee Manager",
+        picture: "",
+        used: "Node, SQL, Console Table, Inquirer",
+        repoLink: "https://github.com/RachaelKStokes/human.resources.mini"
+        
+    },
+    {
+        id: "4",
+        title: "E-Commerce Back-End",
+        picture: "",
+        used: "ORM, SQL, Express.js, HTTPIE",
+        repoLink: "https://github.com/RachaelKStokes/human.resources.mini"
+       
+    }
+]
+
+export default projects;
